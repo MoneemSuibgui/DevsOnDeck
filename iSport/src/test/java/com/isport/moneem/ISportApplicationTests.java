152aa37defbf0646025f7459e6cea44ad9df384e
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-02-18 18:24:46 +0100

    ISport application

---
+++ iSport/src/test/java/com/isport/moneem/ISportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isport.moneem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
