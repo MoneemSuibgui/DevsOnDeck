114fe39ef74c6a448529ebfbab72354d4aa2401a
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-10 23:38:48 +0100

    Devs_on_Deck app

---
+++ src/test/java/com/devs_on_deck/moneem/DevsOnDeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs_on_deck.moneem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsOnDeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
