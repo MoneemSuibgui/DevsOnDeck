f6d4e36eb0e9407485e73d84cef53ce4a298689e
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-10 23:25:39 +0100

    Create LoginUser  class to check the logged user

---
+++ src/main/java/com/devs_on_deck/moneem/models/LoginUser.java
@@ -0,0 +1,42 @@
+package com.devs_on_deck.moneem.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public class LoginUser {
+	
+	// member variables
+	@Email
+	@NotEmpty(message="* Email is required ")
+	private String email;
+	
+	@NotEmpty
+	@Size(min=8,max=20,message="* Password must between 8 and 20 characters")
+	private String password;
+	
+	// beans constructor
+	public LoginUser() {}
+
+	
+	// getters & setters
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}
