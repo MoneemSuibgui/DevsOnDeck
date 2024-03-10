dae0ec9446e70b86ca13306804e1d6bbb18640b4
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-10 23:41:34 +0100

    Create controller to handles requests & interact with the views and models to process data and send response to client

---
+++ src/main/java/com/devs_on_deck/moneem/controllers/OrganizationController.java
@@ -0,0 +1,162 @@
+package com.devs_on_deck.moneem.controllers;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.devs_on_deck.moneem.models.LoginOrg;
+import com.devs_on_deck.moneem.models.Organization;
+import com.devs_on_deck.moneem.models.Position;
+import com.devs_on_deck.moneem.models.User;
+import com.devs_on_deck.moneem.services.OrgService;
+import com.devs_on_deck.moneem.services.PositionService;
+import com.devs_on_deck.moneem.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class OrganizationController {
+	
+	@Autowired
+	private OrgService service;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private PositionService positionService;
+	
+	private static ArrayList<User> availableDev=new ArrayList<User>();
+	
+	// Display route : Organization form
+	@GetMapping("/orgs/register")
+	public String orgForm(@ModelAttribute("organization")Organization org) {
+		return "orgRegister.jsp";
+	}
+	
+	// Action route : Register Organization
+	@PostMapping("/orgs/register")
+	public String register(@Valid @ModelAttribute("organization")Organization org,BindingResult result,HttpSession session) {
+		
+		this.service.register(org, result);
+		if(result.hasErrors()) {
+			return "orgRegister.jsp";
+		}
+		Organization organization=this.service.create(org);
+		session.setAttribute("orgId", organization.getId());
+		
+		return "redirect:/orgs/dashboard";
+	}
+	
+	
+	// Display route login form organization
+	@GetMapping("/orgs/login")
+	public String loginForm(@ModelAttribute("org") Organization org) {
+		return "orgLogin.jsp"; 
+	}
+	// Action route login organization
+	@PostMapping("/orgs/login")
+	public String login(@Valid @ModelAttribute("org") LoginOrg org,BindingResult result,HttpSession session) {
+		Organization loginOrg=this.service.login(org, result);
+		if(result.hasErrors()) {
+			return "orgLogin.jsp";
+		}
+		session.setAttribute("orgId", loginOrg.getId());
+		return "redirect:/orgs/dashboard";
+	}
+	
+	// Display route :render dashboard page
+	@GetMapping("/orgs/dashboard")
+	public String dashboard(Model model,HttpSession session) {
+		if(session.getAttribute("orgId")!=null) {
+		model.addAttribute("org",this.service.oneOrg((Long)session.getAttribute("orgId")));
+		List<User> developers=userService.all();
+		model.addAttribute("developers", developers);
+		model.addAttribute("jobOffers", positionService.all());
+		
+		return "dashboard.jsp";
+		}return "redirect:/orgs/register";
+	}
+	
+	// Display route : position form
+	@GetMapping("/orgs/jobs/new")
+	public String newPosition(@ModelAttribute("position") Position position,HttpSession session) {
+		if(session.getAttribute("orgId")!=null) {
+			return "newPosition.jsp";
+		}
+		return "redirect:/orgs/register";		
+	}
+	
+	// Action Route save the position to database
+	@PostMapping("/orgs/jobs/new")
+	public String savePosition(@Valid @ModelAttribute("position") Position position,BindingResult result,Model model,HttpSession session) {
+		if(result.hasErrors()) {
+			return "newPosition.jsp";
+		}
+		Long orgId=(Long)session.getAttribute("orgId");
+		Organization org=this.service.oneOrg(orgId);
+		position.setOrganization(org);
+		positionService.add(position);
+		return "redirect:/orgs/dashboard";	
+		
+	}
+	
+	@GetMapping("/offers/jobs/{offerId}")
+	public String chooseDevelopers(@PathVariable("offerId") Long offerId,Model model,HttpSession session) {
+		model.addAttribute("org", this.service.oneOrg((Long)session.getAttribute("orgId")));
+		model.addAttribute("position",positionService.getById(offerId));
+		List<Position> positions=positionService.all();
+		List<User> developers=userService.all();
+		
+		
+		for(User developer:developers) {
+			for(Position position:positions) {				
+				Boolean isDevs=getAvailableDevelopers(developer.getProfile().getLanguages(),
+													   developer.getProfile().getFrameworks(),
+													   position.getSkills());
+				if(isDevs) {
+					model.addAttribute("developers", developers);
+				}
+			}
+		}
+		
+		return "specificDeveolpers.jsp";
+	}
+	
+
+
+	@SuppressWarnings("unused")
+	private Boolean getAvailableDevelopers(ArrayList<String> languages, ArrayList<String> frameworks,
+			ArrayList<String> skills) {
+		Boolean isExist = false;
+			for(int i=0;i<languages.size();i++) {
+				for(int j=0; j<frameworks.size();j++) {
+					if(skills.contains(languages.get(i)) || skills.contains(frameworks.get(j))) {
+						return  true;
+					}return true;
+				}
+		}
+		System.out.println(isExist);
+		return isExist;
+	}
+
+	// route for Organization logout
+	@GetMapping("/orgs/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("orgId");
+		return "redirect:/orgs/register";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/devs_on_deck/moneem/controllers/UserController.java
@@ -0,0 +1,213 @@
+package com.devs_on_deck.moneem.controllers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.devs_on_deck.moneem.models.LoginUser;
+import com.devs_on_deck.moneem.models.Profile;
+import com.devs_on_deck.moneem.models.User;
+import com.devs_on_deck.moneem.services.SkillsService;
+import com.devs_on_deck.moneem.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class UserController {
+	
+	// inject UserService
+	@Autowired
+	private UserService service;
+	
+	// inject SkillsService
+	@Autowired
+	private SkillsService skillService;
+		
+	private static String uploaded_path="src/main/resources/static/pictures/";
+
+	
+	@GetMapping("/")
+	public String home() {
+		return "redirect:/devs/register";
+	}
+	
+	// Display route  developer register from
+	@GetMapping("/devs/register")
+	public String developerForm(@ModelAttribute("user") User user) {
+		return "registerUser.jsp";
+	}
+	
+	// Action route register developer :add developer
+	@PostMapping("/create/developer")
+	public String register(@Valid @ModelAttribute("user") User user,BindingResult result
+							,HttpSession session,@RequestParam("picture") MultipartFile file ,RedirectAttributes redirectAttr) {
+		this.service.register(user, result);
+		if(result.hasErrors()) {
+			return "registerUser.jsp";
+		}
+		// validation for picture of developer
+		if(file.isEmpty() && (file.getOriginalFilename() !=".jpg" || file.getOriginalFilename() !=".png")) {
+			redirectAttr.addFlashAttribute("error", "* Picture cannot be empty, must be jpg or png extension");
+			return "redirect:/devs/register";
+		}
+		try {
+			byte [] bytes=file.getBytes();
+			Path path=Paths.get(uploaded_path+file.getOriginalFilename());
+			Files.write(path, bytes);
+			// get the URL of the file we just uploaded and set to image_url of user belongs to it
+			String url="/pictures/"+file.getOriginalFilename();
+			user.setImage_url(url);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		service.add(user);
+		
+		session.setAttribute("userId", user.getId());
+		return "redirect:/devs/skills/languages";
+
+	}
+	
+	// Display route Login page
+	@GetMapping("/devs/login")
+	public String loginForm(@ModelAttribute("user") User user) {
+		return "loginUser.jsp";
+	}
+	
+	// Action route login
+	@PostMapping("/devs/login")
+	public String login(@Valid @ModelAttribute("user")LoginUser user,BindingResult result,HttpSession session) {
+		User LogedUser=this.service.login(user, result);
+		if(result.hasErrors()) {
+			return "loginUser.jsp";
+		}
+		session.setAttribute("userId", LogedUser.getId());
+		return "redirect:/devs/skills/languages";
+	}
+	
+	@GetMapping("/dashboardJob")
+	public String dashboardJob(Model model,HttpSession session) {
+		
+		User user=this.service.oneUser((Long)session.getAttribute("userId"));
+
+		if(session.getAttribute("userId")!=null ) {
+			model.addAttribute("LGuser", user);
+			return "dashboardJobs.jsp";
+		}
+		return "redirect:/devs/register";
+	}
+	
+	
+	@GetMapping("/devs/skills/languages")
+	public String languages(Model model,HttpSession session) {
+
+		if(session.getAttribute("userId")!= null ) {
+			User user=this.service.oneUser((Long)session.getAttribute("userId"));
+			if(user.getProfile()!= null) {
+				return "redirect:/developer/details";
+			}
+			model.addAttribute("LGuser", user);
+		return "languageSkills.jsp";
+		}return"redirect:/devs/register";
+	}
+	
+	// Action route : save developer skills => languages
+	@PostMapping("/devs/skills/languages")
+	public String saveLanguages(@RequestParam(name="languages") ArrayList<String> lang,
+								@RequestParam(name="biography") String bio,
+								HttpSession session,RedirectAttributes redirectAttribute) {
+		
+		User user=this.service.oneUser((Long)session.getAttribute("userId"));
+		if(lang.size()<5 || lang.size()>5) {
+			redirectAttribute.addFlashAttribute("errorMessage", "* Languages list must be 5 languages");
+			return "redirect:/devs/skills/languages";
+		}
+		Profile profile=new Profile();
+		profile.setDeveloper(user);
+		profile.setBiography(bio);
+		profile.setLanguages(lang);
+		skillService.add(profile);
+		
+		
+		this.service.update(user);
+		return "redirect:/devs/skills/frameworks";
+	}
+	
+	@GetMapping("/devs/skills/frameworks")
+	public String frameworks(Model model,HttpSession session) {
+		if(session.getAttribute("userId")!=null) {
+		User user=this.service.oneUser((Long)session.getAttribute("userId"));
+		model.addAttribute("LGuser", user);
+		return "frameworks.jsp";
+		}return"redirect:/devs/register";
+	}
+	
+	// Action route : save developer skills => frameworks
+		@PostMapping("/devs/skills/frameworks")
+		public String saveFrameworks(
+				@RequestParam(value="frameworks") ArrayList<String> frameworks,
+				@RequestParam(value="profession") String profession,
+				@RequestParam(value="experience") Integer experience,
+				HttpSession session,RedirectAttributes redirectAttribute) {
+			User user=this.service.oneUser((Long)session.getAttribute("userId"));
+			if(frameworks.size()<5 || frameworks.size()>5) {
+				redirectAttribute.addFlashAttribute("errorMessage", "* Languages list must be 5 languages");
+				return "redirect:/devs/skills/languages";
+			}
+			if(experience.toString()== null) {
+				redirectAttribute.addFlashAttribute("errorExperience", "* Experience is required !!");
+				return "redirect:/devs/skills/languages";
+			}
+			Profile profile=skillService.getOne(user.getProfile().getId());
+			profile.setFrameworks(frameworks);
+			profile.setProfession(profession);
+			profile.setExperience(experience);
+			profile.setDeveloper(user);
+			
+			skillService.update(profile);
+			return "redirect:/developer/details";
+		}
+		
+	// show all developer details
+		@GetMapping("/developer/details")
+		public String developerInfo(Model model,HttpSession session) {
+			if(session.getAttribute("userId") == null) {
+				return "redirect:/devs/register";
+			}
+			User user=this.service.oneUser((Long)session.getAttribute("userId"));
+			model.addAttribute("user", user);
+			System.out.println(user);
+			return "developerDetails.jsp";
+			
+		}
+	
+	
+	// logout route
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("userId");
+		return "redirect:/devs/register";
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
